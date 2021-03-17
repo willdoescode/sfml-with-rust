40c5639355c671b303fe5de5420bd4f2e1f3358a
Will Lane <williamlane923@gmail.com>
2021-03-17 00:38:43 -0700

    added sprite scale and sprite position

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use sfml::graphics::{RenderWindow, RenderTarget, Color, Texture, Sprite, RenderStates};
+use sfml::graphics::{RenderWindow, RenderTarget, Color, Texture, Sprite, RenderStates, Transformable};
 use sfml::window::{VideoMode, Style, ContextSettings, Event};
+use sfml::system::Vector2;
 
 fn main() {
 	let mut window = RenderWindow::new(
@@ -10,7 +11,12 @@ fn main() {
 	);
 
 	let texture = Texture::from_file("assets/character.png").unwrap();
-	let sprite = Sprite::with_texture(&texture);
+	let mut sprite = Sprite::with_texture(&texture);
+	sprite.scale(Vector2::new(10.0, 10.0));
+	sprite.set_position(Vector2::new(
+		(window.size().x / 2) as f32,
+		(window.size().y / 2) as f32
+	));
 
 	while window.is_open() {
 		match window.poll_event() {
